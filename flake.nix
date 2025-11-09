ebd8ae25885a5694adf3f7369552e5ec6245cb70
Skullheadx <admonty1@protonmail.com>
2025-11-09 17:19:53 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Personal Website Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          fish
+        ];
+
+        shellHook = ''
+          exec fish
+        '';
+      };
+
+    };
+}
