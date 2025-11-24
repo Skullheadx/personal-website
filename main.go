e2089760c51748a35a2dafe13905fa0cfb46f901
Skullheadx <admonty1@protonmail.com>
2025-11-23 23:58:15 -0500

    serve embedded template

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -10,16 +11,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed assets/*
+//go:embed assets/* templates/*
 var f embed.FS
 
 func main() {
 	router := gin.Default()
 
-	router.StaticFS("/public", http.FS(f))
-	router.StaticFileFS("/favicon.ico", "assets/favicon.ico", http.FS(f))
+	templ := template.Must(template.New("").ParseFS(f, "templates/*.tmpl"))
+	router.SetHTMLTemplate(templ)
 
+	router.StaticFS("/public", http.FS(f))
+
+	router.StaticFileFS("/favicon.ico", "assets/favicon.ico", http.FS(f))
 	router.GET("/health", getHealth)
+	router.GET("/", getMainPage)
 
 	if err := router.Run("localhost:8080"); err != nil {
 		log.Fatal(err)
@@ -28,4 +33,10 @@ func main() {
 
 func getHealth(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Healthy %v", time.Now())})
+}
+
+func getMainPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Main website",
+	})
 }
