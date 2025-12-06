601b54a1c671cfab574cae4f575d0cfd5ce34210
Skullheadx <admonty1@protonmail.com>
2025-12-06 14:04:27 -0500

    figure out how to build go module

--- flake.nix
+++ flake.nix
@@ -17,6 +17,32 @@
           go
           curl
           fish
+          (buildGoModule rec {
+            pname = "pet";
+            version = "0.3.4";
+
+            src = fetchFromGitHub {
+              owner = "knqyf263";
+              repo = "pet";
+              tag = "v${version}";
+              hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
+            };
+
+            vendorHash = "sha256-6hCgv2/8UIRHw1kCe3nLkxF23zE/7t5RDwEjSzX3pBQ=";
+          })
+          # (pkgs.buildGoModule rec {
+          #   pname = "gin-proxy";
+          #   version = "unstable-2024-12-01";
+          #   src = pkgs.fetchFromGitHub {
+          #     owner = "codegangsta";
+          #     repo = "gin";
+          #     rev = "master";
+          #     sha256 = "sha256-TZSpCcFBlXLPE50bYbXPU5ddoVsBG7YGa7oLmKDFBmE="; # ← you will update this
+          #   };
+          #   vendorHash = null;
+          #   doCheck = false;
+          #   modRoot = "/home/andrew/dev/personal-site/<D-;>";
+          # })
         ];
 
         shellHook = ''
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -26,9 +30,28 @@ func main() {
 	router.GET("/health", getHealth)
 	router.GET("/", getMainPage)
 
-	if err := router.Run("localhost:8080"); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router.Handler(),
 	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Server Shutdown ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
 
 func getHealth(c *gin.Context) {
