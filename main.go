e0fcea9ccebaa7a7dbdfcf2d05b75c7fe061d3ca
Skullheadx <admonty1@protonmail.com>
2025-12-07 17:41:27 -0500

    resume + main page

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
 	router.GET("/health", getHealth)
+	router.GET("/", getMainPage)
+	// router.GET("/about", getAboutPage)
 	router.StaticFile("/favicon.ico", "./assets/favicon.ico")
+	router.StaticFile("/assets/resume.pdf", "./assets/Andrew_Montgomery_Resume.pdf")
 	if err := router.Run("localhost:8080"); err != nil {
 		log.Fatal(err)
 	}
@@ -20,4 +25,8 @@ func main() {
 
 func getHealth(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Healthy %v", time.Now())})
+}
+
+func getMainPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
 }
