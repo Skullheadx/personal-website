21c3a560e4b85e1df6e57b7ece656753d086ec3f
Skullheadx <admonty1@protonmail.com>
2025-12-06 19:06:46 -0500

    health endpoint

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello world! I'm andrew")
+	router := gin.Default()
+	router.GET("/health", getHealth)
+	if err := router.Run("localhost:8080"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getHealth(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Healthy %v", time.Now())})
 }
